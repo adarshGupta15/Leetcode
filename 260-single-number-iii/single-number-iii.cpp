3de5e25715b4ab25b081e60b896891df816bc361
adarshGupta15 <ad282242@gmail.com>
2026-06-15 13:50:32 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (78.99%) - LeetSync

--- 260-single-number-iii/single-number-iii.cpp
+++ 260-single-number-iii/single-number-iii.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
-     int n=nums.size();
+     /*int n=nums.size();
      map<int,int>mpp;
      for(int i=0;i<n;i++){
         mpp[nums[i]]++;
@@ -12,6 +12,22 @@ public:
             ans.push_back(it.first);
         }
      }
-     return ans;
+     return ans;*/
+     long long XORR=0;
+     int n=nums.size();
+     for(int i=0;i<n;i++){
+        XORR=XORR^nums[i];
+     }
+     int rightmost=(XORR & -(XORR));
+     int b1=0,b2=0;
+     for(int i=0;i<n;i++){
+        if(nums[i] & rightmost){
+            b1=b1^nums[i];
+        }
+        else{
+            b2=b2^nums[i];
+        }
+     }
+     return {b1,b2};
     }
 };
