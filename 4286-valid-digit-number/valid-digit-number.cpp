fedacbcd806fe623a256555e9d7112a9240a2043
adarshGupta15 <ad282242@gmail.com>
2026-08-05 22:15:13 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (6.64%) - LeetSync

---
+++ 4286-valid-digit-number/valid-digit-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool validDigit(int n, int x) {
+        string s=to_string(n);
+        if(s[0]-'0'==x) return false;
+        for(int i=1;i<s.size();i++){
+            if(s[i]-'0'==x)
+            return true;
+        }
+        return false;
+    }
+};
