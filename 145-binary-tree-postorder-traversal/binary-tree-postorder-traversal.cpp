6301a608560054c8c3b516b52691297d9f9511b7
adarshGupta15 <ad282242@gmail.com>
2026-08-13 22:50:20 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (45.87%) - LeetSync

---
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
@@ -0,0 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int>post;
+        stack<TreeNode*>st;
+
+        TreeNode*curr=root;
+        TreeNode*temp;
+
+        while(curr!=NULL || !st.empty()){
+            if(curr!=NULL){
+                st.push(curr);
+                curr=curr->left;
+            }
+            else{
+                temp=st.top()->right;
+
+                //Right child nahi hai
+                if(temp==NULL){
+                    temp=st.top();
+                    st.pop();
+                    post.push_back(temp->val);
+
+                    // jo nodes ka right subtree already process ho chukaa hain
+                    while(!st.empty() && temp==st.top()->right){
+                        temp=st.top();
+                        st.pop();
+                        post.push_back(temp->val);
+                    }
+                }
+                else{
+                    // right subtree process
+                    curr=temp;
+                }
+            }
+        }
+        return post;
+    }
+};
